c93f39671f3e28590eb3adcdfeedade2394954e1
Gareth Evans <gdevans@gmail.com>
2019-03-31 16:00:57 +0100

    hoisting
    ability to call functions before they have been declared

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,14 +1,49 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// Gloabl Execution Context
 
+/*
+    ______________________
+    | current exectuting |
+    |       function     |
+    ----------------------
+    |  Global Execution  |
+    |      Context       |
+-----------------------------
+Whe global code runs, it is operating in the Global Exectution Conext, as porperties of the global context object.
 
+When a function is executed, it creates a new execution context and stacks ontop of the Global exectuion context. 
 
+When it finishes it is said to return to the next context in the stack and pops off the execution stack.
 
+*/
 
+// Function declaration, can call function before it is created
+calculateAge(1990);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+// Function expression 
+// retirement(1990); (hoisting does not work)
 
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
+// Variables
+
+console.log(age);
+var age = 23;
+console.log(age);
+
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
