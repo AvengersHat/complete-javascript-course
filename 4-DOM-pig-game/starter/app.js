e90d19c522f63f8aba03bc490ba9f36ddfd39404
Gareth Evans <gdevans@gmail.com>
2019-04-08 08:59:20 +0100

    commit
    Work out how to disable buttons

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -16,10 +16,12 @@ activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none'; //Use dot notation for classes. to change css use the .style method and the .property This is to hide the dice on startup
 
+
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
+
 
 
 //Anonymous function to roll the dice
@@ -43,7 +45,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
         
     } else {
-        resetPlayer();
+        switchPlayer();
         /*
         //Next player - Using ternary function condition ? action : else
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -72,7 +74,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
     // 2. Finish player turn
-    resetPlayer();
+    //switchPlayer();
     /*
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0; // roundScore reset to 0 for next player
@@ -85,9 +87,20 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     
         document.querySelector('.dice').style.display = 'none';
         */
+    
+    // 3. Check if player won the game
+    if (scores[activePlayer] >= 100) {
+        //won game
+        //alert('Player ' + activePlayer + ' won the game');
+        document.getElementById('name-' + activePlayer).textContent = 'WINNER';
+        // MAKE THE ROLL  AND HOLD BUTTONS INACTIVE
+        //document.getElementById('.btn-roll').disabled = true;
+    } else {
+        switchPlayer();
+    }
 })
 
-function resetPlayer() {
+function switchPlayer() {
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0; // roundScore reset to 0 for next player
         
@@ -100,7 +113,22 @@ function resetPlayer() {
         document.querySelector('.dice').style.display = 'none';
 }
 
+//Add new game button
 
+
+document.querySelector('.btn-new').addEventListener('click', function() {
+    
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'PLAYER 1';
+    document.getElementById('name-1').textContent = 'PLAYER 2';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    
+})
 
 
 
